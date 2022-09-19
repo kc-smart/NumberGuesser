3b2628c1633e6fc01060d2107e4ae210f56cd072
Kayla Smart <kaylacsmart@gmail.com>
2022-09-19 19:57:13 -0400

    Option to replay game. OutOfRangeException>higher.

--- Program.cs
+++ Program.cs
@@ -7,7 +7,8 @@ namespace NumberGuesser
     {
         static void Main(string[] args)
         {
-            Console.Write("Welcome... \nI am a number guessing Wizard. ");
+            greeting();
+
 
             //keep track of questions
             int questions = 0;
@@ -17,17 +18,83 @@ namespace NumberGuesser
             int guess = 0;
             //player answer
             string answer = "";
+            //keep track of number of games
+            int numberOfGames = 0;
+            //keep track of average guesses
+            //int averageGuesses = 0;
 
+            createList(numbers);
+
+            Console.WriteLine($"Please think of a number from {numbers[0]} to {numbers.Count}.\nI will guess your number by asking you a few questions.\n");
+
+            //gamePlay();
+
+            do
+            {
+                numberOfGames++;
+                //Select random number from list
+                guess = randomGuess(numbers);
+
+                //ask if higher, lower, or correct
+                Console.WriteLine($"My guess is {guess}. Is your number higher, lower, or am I correct?");
+
+            top:
+                answer = Console.ReadLine();
+
+                //ensure response is all lower before moving on
+                answer.ToLower();
+
+                questions++;
+
+                if (answer == "higher")
+                {
+                    removeLower(numbers, guess);
+                    //Status
+                    status(numbers, questions);
+                }
+                else if (answer == "lower")
+                {
+                    removeHigher(numbers, guess);
+                    //Status
+                    status(numbers, questions);
+                }
+                else if (answer == "correct")
+                {
+                    Console.WriteLine($"I correctly guessed your number in {questions} tries. Amazing!");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid response. Please enter higher, lower, or correct.");
+                    goto top;
+                }
+            } while (answer != "correct");
+
+
+            endOfGame();
+
+
+        }
+
+        static void greeting()
+        {
+            Console.Write("Welcome... \nI am a number guessing Wizard. ");
+        }
+
+        static List<int> createList(List<int> numbers)
+        {
             for (int i = 1; i < 1025; i++)
             {
                 numbers.Add(i);
             }
 
-            Console.WriteLine($"Please think of a number from {numbers[0]} to {numbers.Count}.\nI will guess your number by asking you a few questions.\n");
+            return numbers;
+        }
 
-
+        /*
+        gamePlay(){
             do
             {
+            numberOfGames++;
             //Select random number from list
             guess = randomGuess(numbers);
 
@@ -55,77 +122,67 @@ namespace NumberGuesser
                 Console.WriteLine("Invalid response. Please enter higher, lower, or correct.");
             }
             } while (answer != "correct");
-            
-
-            Console.WriteLine("Thank you for playing.");
-        
-
         }
+         */
 
-        static int status(List<int> numbers, int questions){
-            Console.WriteLine($"{numbers.Count} numbers remaining.");
-            questions++;
-            Console.WriteLine($"I have asked {questions} questions.");
-            return questions;
-        }
-
-/* 
-        public List<Int> removeEven(numbers){
-
-            for(int i; i < numbers.length; i++){
-                if(numbers[i] % 2 = 0){
-                    number.RemoveAt(i);
-                    i++;
-                }
-                else
-                {
-                    i++;
-                }
-            }
-
-            return numbers;
-        } 
-
-        public List<Int> removeOdd(numbers){
-            for(int i; i < numbers.length; i++){
-                if(numbers[i] % 2 != 0){
-                    number.RemoveAt(i);
-                    i++;
-                }
-                else
-                {
-                     i++;
-                }
-            }
-
-            return numbers;
-        }
-        */
-
-        static int randomGuess(List<int> numbers){
+        static int randomGuess(List<int> numbers)
+        {
             //draw random number from 1-1024 using guess var
             var rnd = new Random();
             int numIndex = rnd.Next(numbers.Count);
-            //guess = numbers[numIndex];
             return numbers[numIndex];
         }
 
-        static List<int> removeHigher(List<int> numbers, int guess){
-            for(int i = guess; i < numbers.Count; i++){
-                numbers.RemoveAt(i);
-                    //i++;
-            }
+        static List<int> removeHigher(List<int> numbers, int guess)
+        {
+            Console.WriteLine($"I will now remove all numbers at and above {guess}");
+
+            int range = numbers.Count - guess;
+            int min = guess - 1;
+
+            numbers.RemoveRange(min, range);
 
             return numbers;
         }
 
-        static List<int> removeLower(List<int> numbers, int guess){
-            for(int i = guess; i > -1; i--){
-                numbers.RemoveAt(i);
-                //i--;
-            }
+        static List<int> removeLower(List<int> numbers, int guess)
+        {
+            Console.WriteLine($"I will now remove all numbers at and below {guess}");
+
+            numbers.RemoveRange(0, guess);
 
             return numbers;
+        }
+
+        static void status(List<int> numbers, int questions)
+        {
+            Console.WriteLine(numbers.Count + " numbers remaining.");
+            Console.WriteLine($"I have asked {questions} questions.");
+        }
+
+        static void endOfGame()
+        {
+            Console.WriteLine("Thank you for playing. Would you like to play again?\n");
+
+        top:
+            string playAgain = Console.ReadLine();
+
+            //ensure response is all lower before moving on
+            playAgain.ToLower();
+
+            if (playAgain == "yes")
+            {
+                Console.WriteLine("We need to figure this out!");
+            }
+            else if (playAgain == "no")
+            {
+                Console.WriteLine("Come back soon!");
+            }
+            else
+            {
+                Console.WriteLine("Invalid response. Please enter yes or no.");
+                goto top;
+            }
         }
 
     }
